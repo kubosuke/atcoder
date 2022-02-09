9d93b5ccd1a59f09d4b95a7a091ef1b890d2bc6e
Keisuke Kubota <Kk.bb1130@outlook.jp>
2022-02-09 10:36:48 +0900

    Add. comments ABC238 d

--- abc238/src/d.rs
+++ abc238/src/d.rs
@@ -1,3 +1,40 @@
+// https://stackoverflow.com/questions/36477623/given-an-xor-and-sum-of-two-numbers-how-to-find-the-number-of-pairs-that-satisf
+//  010
+//  111
+// +---
+// 1001
+//
+// + operator : 0+0 = 0, 0+1 = 1, 1+0 = 1, 1+1 = 0
+// + operator is very similar to XOR operator
+//
+//    010
+//    111
+// XOR---
+//    101
+//
+// only the difference is carry-in bit
+// if the previous bits for a,b are 1, then we add it also
+// This is exactly (a AND b) * 2
+//
+//    010
+//    111
+// AND---
+//    010
+//
+//
+// 101(a XOR b) + 010(a AND b) * 2 = 1001(a + b)
+//
+// we have (a+b) as s, (a AND b) as a, so that the formula are:
+//
+// a XOR b = s - 2 * a
+//
+// a and b should be positive, so
+//
+// (1) s - 2 * a >= 0
+// (2) (s - 2 * a) & a == 0 ('cause (x XOR y) AND (x AND y) is 0)
+//
+// we just solve them, and if x,y are not satisfy above restrictions, return No, else Yes
+
 use proconio::input;
 use std::io::{self, Write};
 
